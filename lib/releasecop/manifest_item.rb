e6397b4565cbce48082847cdf62849f20c4b542d
Joey Aghion <joey@aghion.com>
2018-11-14 22:33:04 -0500

    Allow aws_... configs in manifest items

--- lib/releasecop/manifest_item.rb
+++ lib/releasecop/manifest_item.rb
@@ -1,6 +1,6 @@
 module Releasecop
   class ManifestItem
-    OPTION_KEYS = %w[hokusai tag_pattern]
+    OPTION_KEYS = %w[hokusai tag_pattern, aws_access_key_id, aws_secret_access_key]
     attr_reader :git, :branch, :name
 
     def initialize(repo_name, item)
@@ -24,13 +24,24 @@ module Releasecop
     private
 
     def find_hokusai_sha
-      images_output = `hokusai registry images`
+      images_output = with_aws_env { `hokusai registry images` }
       tags = images_output.lines.grep(/\d{4}.* | .* | .*/).map{|l| l.split(' | ').last.split(',').map(&:strip)}
       tags.detect{|t| t.include?(@options['hokusai']) }.detect{|t| t[/^[0-9a-f]{40}$/]}
     end
 
     def find_tag_pattern_sha
       `git for-each-ref --format='%(objectname)' --count=1 --sort=-authordate --sort=-committerdate 'refs/tags/#{@options['tag_pattern']}'`.strip
+    end
+
+    # not thread-safe
+    def with_aws_env(&block)
+      ENV.update(
+        'AWS_ACCESS_KEY_ID' => @options['aws_access_key_id'],
+        'AWS_SECRET_ACCESS_KEY' => @options['aws_secret_access_key']
+      )
+      yield
+    ensure
+      ENV.update('AWS_ACCESS_KEY_ID' => nil, 'AWS_SECRET_ACCESS_KEY' => nil)
     end
   end
 end
